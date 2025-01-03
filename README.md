46c75104108ccb68e94765f52d58af76e8431792
bradyflynn240 <28bradyflynn@sau11.org>
2025-01-03 14:10:27 -0500

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,33 @@
+ 
+
+
+> Open this page at [https://bradyflynn240.github.io/brady-flynn/](https://bradyflynn240.github.io/brady-flynn/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/bradyflynn240/brady-flynn** and import
+
+## Edit this project ![Build status badge](https://github.com/bradyflynn240/brady-flynn/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/bradyflynn240/brady-flynn** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/bradyflynn240/brady-flynn/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,73 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . 3 . . . . . . . . . . 
+        . . . . . 3 . . . . . . . . . . 
+        . . . . . 3 . . . . . . . . . . 
+        `, 50, -50)
+    music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.UntilDone)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
+    other_sprint.startEffect(effects.fire)
+    scene.cameraShake(4, 500)
+    info.changeScoreBy(-1)
+})
+let Astroid: Sprite = null
+let projectile: Sprite = null
+let other_sprint: Sprite = null
+effects.starField.startScreenEffect()
+other_sprint = sprites.create(img`
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c b . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . c 7 . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . 8 7 . . . . . . . 
+    . . . . . . 8 8 5 6 . . . . . . 
+    . . . . . . 8 7 5 6 . . . . . . 
+    . . . . . c c c 6 6 6 . . . . . 
+    . . . . 8 8 7 7 7 5 6 6 . . . . 
+    . . 8 f f f c c 6 6 f f 6 6 . . 
+    . 8 8 8 8 6 6 7 7 7 7 5 7 6 6 . 
+    8 8 8 8 8 8 6 6 7 7 7 5 7 7 6 6 
+    8 8 8 8 8 8 6 6 7 7 7 7 5 7 6 6 
+    `, SpriteKind.Player)
+controller.moveSprite(other_sprint, 100, 0)
+other_sprint.setPosition(77, 112)
+other_sprint.setStayInScreen(true)
+game.onUpdateInterval(1000, function () {
+    Astroid = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 6 6 6 6 . . . . . . 
+        . . . . 6 6 6 5 5 6 6 6 . . . . 
+        . . . 7 7 7 7 6 6 6 6 6 6 . . . 
+        . . 6 7 7 7 7 8 8 8 1 1 6 6 . . 
+        . . 7 7 7 7 7 8 8 8 1 1 5 6 . . 
+        . 6 7 7 7 7 8 8 8 8 8 5 5 6 6 . 
+        . 6 7 7 7 8 8 8 6 6 6 6 5 6 6 . 
+        . 6 6 7 7 8 8 6 6 6 6 6 6 6 6 . 
+        . 6 8 7 7 8 8 6 6 6 6 6 6 6 6 . 
+        . . 6 8 7 7 8 6 6 6 6 6 8 6 . . 
+        . . 6 8 8 7 8 8 6 6 6 8 6 6 . . 
+        . . . 6 8 8 8 8 8 8 8 8 6 . . . 
+        . . . . 6 6 8 8 8 8 6 6 . . . . 
+        . . . . . . 6 6 6 6 . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, 0, 50)
+    Astroid.x = randint(0, 160)
+    other_sprint.setKind(SpriteKind.Enemy)
+})
